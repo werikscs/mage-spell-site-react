d5e5f4d86f1be635155436d87e98bfe06b14ffc6
Weriks Santos <werikscs@gmail.com>
2022-10-29 18:16:24 -0300

    feat: create theme style

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App'
+import GlobalStyle from './styles/global-style'
+import { light } from './styles/theme-style'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/styles/theme-style.ts
@@ -0,0 +1,15 @@
+const light = {
+  title: "light",
+  colors: {
+    primary: "#E9F0F2",
+    secondary: "#FCFCFC",
+    terciary: "#C1D2D9",
+    text: "#394D59",
+    detailText: "#547A92",
+    divider: "#6D90A625",
+  },
+};
+
+const dark = {};
+
+export { light, dark };
