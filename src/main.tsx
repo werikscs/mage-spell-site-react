eaeea40666850b89078acbbf9696a708a3987dca
Weriks Santos <werikscs@gmail.com>
2022-11-02 20:23:11 -0300

    refactor: implement main provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeContext, ThemeProvider } from 'styled-components'
 import App from './App'
-import ToogleThemeContext from './context/ToogleThemeContext'
-import ToogleThemeProvider from './providers/ThemeProvider'
+import Providers from './providers'
 import GlobalStyle from './styles/global-style'
-import { dark, light } from './styles/themes-style'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ToogleThemeProvider>
-      <ToogleThemeContext.Consumer>
-        {
-          (value) => (
-            <ThemeProvider theme={value.theme === 'light' ? light : dark}>
-              <GlobalStyle />
-              <App />
-            </ThemeProvider>
-          )
-        }
-      </ToogleThemeContext.Consumer>
-    </ToogleThemeProvider>
+      <Providers>
+        <GlobalStyle />
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>
 )
