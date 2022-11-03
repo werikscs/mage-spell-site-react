e7cb201bbd3a90ea0dc821867cebf27c470a8e80
Weriks Santos <werikscs@gmail.com>
2022-11-02 21:14:19 -0300

    feat: create menu button component

---
+++ src/components/MenuButton/index.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import IconMenuDark from '../../assets/toggle-menu-dark.svg'
+import IconMenuLight from '../../assets/toggle-menu-light.svg'
+import ToggleMenuContext from "../../context/ToggleMenuContext"
+import ToogleThemeContext from "../../context/ToggleThemeContext"
+import { StyledButton } from "./style"
+
+const MenuButton = () => {
+  const {toggleMenu} = useContext(ToggleMenuContext)
+  const {theme} = useContext(ToogleThemeContext)
+
+  return (
+    <StyledButton
+      onClick={toggleMenu}
+    >
+      <img
+        src={theme === 'light' ? IconMenuLight : IconMenuDark}
+        alt=""
+      />
+    </StyledButton>
+  )
+}
+
+export default MenuButton
---
+++ src/components/MenuButton/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: transparent;
+
+  > img {
+    pointer-events: none;
+  }
+`;
