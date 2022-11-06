fda9c03996e4ed9cf073ab7fe3a229681ab279e7
Weriks Santos <werikscs@gmail.com>
2022-11-06 20:16:44 -0300

    feat: create search bar component

---
+++ src/components/SearchBar/style.ts
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion"
+import styled from "styled-components"
+
+const StyledDiv = styled(motion.div)`
+  width: 100%;
+  height: fit-content;
+  max-height: 30px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+
+  border-radius: 8px;
+  border: 1px solid ${({theme}) => theme.colors.text};
+
+  background-color: ${({theme}) => theme.colors.primary};
+
+  > input {
+    width: 100%;
+
+    padding: 7px 0;
+    padding-left: 8px;
+
+    color: ${({theme}) => theme.colors.text};
+
+    ::placeholder{
+      color: ${({theme}) => theme.colors.text};
+    }
+
+    background-color: transparent;
+  }
+`
+
+export default StyledDiv
