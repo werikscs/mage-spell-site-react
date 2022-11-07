beef885512ed31767df14feca8ff8cc5e25f4f2a
Weriks Santos <werikscs@gmail.com>
2022-11-06 22:09:46 -0300

    feat: add desktop version to side menu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,40 +1,45 @@
 import { useContext } from "react"
 import { useTheme } from "styled-components"
 import ToggleMenuContext from "../../context/ToggleMenuContext"
+import WindowSizeContext from "../../context/WindowSizeContext"
 import SearchBar from "../SearchBar"
 import {StyledDiv} from "./style"
 import {StyledSection} from './style'
 
 const SideMenu = (): JSX.Element => {
-  const {isMenuOpened} = useContext(ToggleMenuContext)
+  const { isMenuOpened } = useContext(ToggleMenuContext)
+  const { width } = useContext(WindowSizeContext)
   const theme = useTheme()
 
   const variants = {
     visible: { 
       opacity: 1,
       width: theme.sizes.sideMenuShadow, 
-      transition: { 
-        duration: 0.25,
-      },
+      transition: { duration: 0.25 },
     },
     hidden: {
       opacity: 0,
       width: theme.sizes.sideMenuShadow,
-      transition: { 
-        duration: 0.25,
-      } 
-    }
+      transition: { duration: 0.25 } 
+    },
+    none: { opacity: 1, width: theme.sizes.sideMenu }
   };
 
   const child = {
     hidden: { display: 'none', width: 0, transition: { duration: theme.animation.fast } },
     visible: { display: 'flex', width: theme.sizes.sideMenu, transition: { duration: theme.animation.fast } },
+    none: { display: 'flex', width: theme.sizes.sideMenu }
+  }
+
+  const handleResize = () => {
+    if(width > theme.sizes.maxMobileScreen) return 'none'
+    return isMenuOpened ? 'visible' : 'hidden'
   }
   
   return  (
     <StyledDiv
-      animate={isMenuOpened ? 'visible' : 'hidden'}
-      initial='hidden'
+      animate={handleResize()}
+      initial={handleResize()}
       variants={variants}
     >
       <StyledSection
--- src/components/SideMenu/style.ts
+++ src/components/SideMenu/style.ts
@@ -1,13 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { motion } from 'framer-motion'
 
 const StyledDiv = styled(motion.div)`
-  position: absolute;
-  top: 0px;
-  right: 0;
-  z-index: 0;
+  ${({theme}) => innerWidth <= theme.sizes.maxMobileScreen ? (
+    css`
+      position: absolute;
+      top: 0px;
+      right: 0;
+      z-index: 0;
 
-  width: ${({ theme }) => theme.sizes.sideMenuShadow};
+      width: ${({ theme }) => theme.sizes.sideMenuShadow};
+    `
+  ) : (
+    css`
+      position: static;
+      margin-left: auto;
+
+      width: ${({ theme }) => theme.sizes.sideMenu};
+    `
+  ) }
+  
+
   height: calc(100vh - ${({ theme }) => theme.sizes.headerHeight});
 
   background-color: ${({ theme }) => theme.colors.shadowMenu};
