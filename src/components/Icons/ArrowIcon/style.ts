adafff9d6f6b0c41a264bcdb9e0207390e0a58c8
Weriks Santos <werikscs@gmail.com>
2022-11-09 21:56:04 -0300

    feat: create arrow icon component

---
+++ src/components/Icons/ArrowIcon/style.ts
@@ -0,0 +1,9 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+const StyledDiv = styled(motion.div)`
+  display: flex;
+  align-items: center;
+`;
+
+export default StyledDiv;
