93efe9c0fd2d1db28ffb44f531f52df06dabf8f5
Weriks Santos <werikscs@gmail.com>
2022-11-09 23:23:33 -0300

    feat: extract logo component from header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,12 @@
-import { useContext } from 'react';
-import ToggleThemeContext from '../../context/ToggleThemeContext';
 import ThemeSwitcherButton from '../ToggleThemeButton';
 import MenuButton from '../MenuButton';
 import { StyledHeader, StyledNavHeader } from './style';
-import logoDark from '../../assets/logo-dark.svg';
-import logoLight from '../../assets/logo-light.svg';
+import LogoIcon from '../Icons/LogoIcon';
 
 function Header(): JSX.Element {
-  const { themeType } = useContext(ToggleThemeContext);
-
   return (
     <StyledHeader>
-      <a href="#">
-        <img
-          src={themeType === 'light' ? logoLight : logoDark}
-          alt="site logo"
-        />
-      </a>
+      <LogoIcon />
       <StyledNavHeader>
         <ThemeSwitcherButton />
         <MenuButton />
