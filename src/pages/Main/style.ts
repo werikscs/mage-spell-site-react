81c0c9c80b9532cd69c4a71bbbd0ecb3d8dc1d20
Weriks Santos <werikscs@gmail.com>
2022-11-13 00:04:37 -0300

    feat: change main page to handle list card and side menu

--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
+  display: flex;
+  justify-content: space-between;
+
   position: relative;
 
   width: 100%;
-
-  display: flex;
-  justify-content: space-between;
 `;
 
 export default StyledMain;
