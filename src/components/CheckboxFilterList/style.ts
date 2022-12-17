ccf4797fb2092d043b7252c9aa7fc01d64a9193f
Weriks Santos <werikscs@gmail.com>
2022-12-17 19:51:15 -0300

    refactor: remove unused styles

--- src/components/CheckboxFilterList/style.ts
+++ src/components/CheckboxFilterList/style.ts
@@ -1,7 +1,6 @@
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-const StyledUl = styled(motion.ul)`
+const StyledUl = styled.ul`
   display: flex;
   flex-direction: column;
   width: 100%;
--- src/components/ExpandableElement/index.tsx
+++ src/components/ExpandableElement/index.tsx
@@ -1,6 +1,4 @@
-import { AnimatePresence } from 'framer-motion';
 import { ReactNode, useState } from 'react';
-import useCustomTheme from '../../hooks/useCustomTheme';
 import ArrowIcon from '../Icons/ArrowIcon';
 import { StyledDiv, StyledExpandableDiv, StyledSection } from './style';
 
@@ -13,23 +11,7 @@ function ExpandableElement({
   title,
   children,
 }: IExpandableElement): JSX.Element {
-  const { theme } = useCustomTheme();
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
-  const variants = {
-    hidden: {
-      opacity: 0,
-      transition: {
-        duration: theme.animation.slightlyFast,
-      },
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        duration: theme.animation.slightlyFast,
-      },
-    },
-  };
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -41,18 +23,7 @@ function ExpandableElement({
         <span>{title}</span>
         <ArrowIcon isExpanded={isExpanded} />
       </StyledDiv>
-      <AnimatePresence>
-        {isExpanded && (
-          <StyledExpandableDiv
-            initial="hidden"
-            animate="visible"
-            variants={variants}
-            exit="hidden"
-          >
-            {children}
-          </StyledExpandableDiv>
-        )}
-      </AnimatePresence>
+      {isExpanded && <StyledExpandableDiv>{children}</StyledExpandableDiv>}
     </StyledSection>
   );
 }
