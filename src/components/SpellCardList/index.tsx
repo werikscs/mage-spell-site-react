036721485a77c57c70969278f792dae7dc88def6
Weriks Santos <werikscs@gmail.com>
2022-12-28 11:46:47 -0300

    refactor: separate files from spell list component

--- src/components/SpellCardList/index.tsx
+++ src/components/SpellCardList/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import WindowSizeContext from '../../context/WindowSizeContext';
 import SpellCardLi from '../SpellCardLi';
 import SpellCardFormatter from '../SpellCardLi/spellCardFormatter';
@@ -8,46 +8,20 @@ import spells from '../../utils/spells';
 import StyledMasonry from './style';
 import { calcCollumsCount, calcDinamicWidth } from './utils';
 import { ISpellData } from '../../interfaces-types/interfaces';
-
-export interface IInfiniteScrollProps {
-  callback: () => void;
-}
-
-export function InfiniteScroll({ callback }: IInfiniteScrollProps) {
-  const divInfiniteScrollRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const intersectionObserver = new IntersectionObserver(([entry]) => {
-      const ratio = entry.intersectionRatio;
-
-      if (ratio > 0) {
-        callback();
-      }
-    });
-
-    if (divInfiniteScrollRef.current) {
-      intersectionObserver.observe(divInfiniteScrollRef.current);
-    }
-
-    return () => {
-      intersectionObserver.disconnect();
-    };
-  }, [divInfiniteScrollRef]);
-
-  return <div ref={divInfiniteScrollRef} />;
-}
+import InfiniteScroll from './infinityScroll';
 
 function SpellCardList(): JSX.Element {
   const { width, height } = useContext(WindowSizeContext);
 
   const [spellList, setSpellList] = useState<ISpellData[]>([]);
-  const [slicedSpells, setSlicedSpells] = useState<ISpellData[]>([])
+  const [slicedSpells, setSlicedSpells] = useState<ISpellData[]>([]);
   const [page, setPage] = useState(0);
 
+  const SPELLS_PER_PAGE = 10;
+
   useEffect(() => {
-    setSlicedSpells(spells.slice(page, page + 10));
+    setSlicedSpells(spells.slice(page, page + SPELLS_PER_PAGE));
     setSpellList([...spellList, ...slicedSpells]);
-    console.log(slicedSpells, page);
   }, [page]);
 
   return (
@@ -63,9 +37,13 @@ function SpellCardList(): JSX.Element {
           spellData={SpellCardFormatter.formatDataCardObject(spell)}
         />
       ))}
-      {slicedSpells.length && <InfiniteScroll
-        callback={() => setPage((previousPage) => previousPage + 10)}
-      />}
+      {slicedSpells.length && (
+        <InfiniteScroll
+          callback={() =>
+            setPage((previousPage) => previousPage + SPELLS_PER_PAGE)
+          }
+        />
+      )}
     </StyledMasonry>
   );
 }
