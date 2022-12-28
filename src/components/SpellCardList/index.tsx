2497b89bd3e0b6eea66dd32dfce06d0189adce21
Weriks Santos <werikscs@gmail.com>
2022-12-27 21:44:56 -0300

    feat: add infinite scroll

--- src/components/SpellCardList/index.tsx
+++ src/components/SpellCardList/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import WindowSizeContext from '../../context/WindowSizeContext';
 import SpellCardLi from '../SpellCardLi';
 import SpellCardFormatter from '../SpellCardLi/spellCardFormatter';
@@ -7,9 +7,48 @@ import spells from '../../utils/spells';
 
 import StyledMasonry from './style';
 import { calcCollumsCount, calcDinamicWidth } from './utils';
+import { ISpellData } from '../../interfaces-types/interfaces';
+
+export interface IInfiniteScrollProps {
+  callback: () => void;
+}
+
+export function InfiniteScroll({ callback }: IInfiniteScrollProps) {
+  const divInfiniteScrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(([entry]) => {
+      const ratio = entry.intersectionRatio;
+
+      if (ratio > 0) {
+        callback();
+      }
+    });
+
+    if (divInfiniteScrollRef.current) {
+      intersectionObserver.observe(divInfiniteScrollRef.current);
+    }
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, [divInfiniteScrollRef]);
+
+  return <div ref={divInfiniteScrollRef} />;
+}
 
 function SpellCardList(): JSX.Element {
   const { width, height } = useContext(WindowSizeContext);
+
+  const [spellList, setSpellList] = useState<ISpellData[]>([]);
+  const [slicedSpells, setSlicedSpells] = useState<ISpellData[]>([])
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    setSlicedSpells(spells.slice(page, page + 10));
+    setSpellList([...spellList, ...slicedSpells]);
+    console.log(slicedSpells, page);
+  }, [page]);
 
   return (
     <StyledMasonry
@@ -18,12 +57,15 @@ function SpellCardList(): JSX.Element {
       screenDimensions={{ width, height }}
       style={{ width: calcDinamicWidth(width) }}
     >
-      {spells.map((spell) => (
+      {spellList.map((spell) => (
         <SpellCardLi
           key={spell.id}
           spellData={SpellCardFormatter.formatDataCardObject(spell)}
         />
       ))}
+      {slicedSpells.length && <InfiniteScroll
+        callback={() => setPage((previousPage) => previousPage + 10)}
+      />}
     </StyledMasonry>
   );
 }
